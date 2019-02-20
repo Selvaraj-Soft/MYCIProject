21e59ed80aa95dc7bb5a436a3179baac3c83ffe7
selvaraj <selvarajd@softcrylic.co.in>
2019-02-20 20:40:25 +0530

    Add 1 more class file "Enum_WK"

---
+++ Enum_WK.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topics
+{
+   public class Enum_WK
+    {
+        enum Sequence {LevelA=90, LevelB, LevelC, LevelD, LevelE, LevelF};
+
+        enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void method()
+        {
+            Console.WriteLine(Enum.GetName(typeof(WeekDays), 4+"<-----Using Getname() method"));
+
+            Console.WriteLine("WeekDays constant names:");
+
+            foreach (string str in Enum.GetNames(typeof(WeekDays)))
+            {
+                Console.WriteLine(str+"Iterating the Values in loop");
+            }
+            Console.WriteLine("Enum.TryParse():");
+
+        }
+
+        public static void Main(string[] args)
+        {
+            int status = (int)Sequence.LevelD;
+            Console.WriteLine(status);
+            Console.WriteLine(Sequence.LevelC);
+
+            method();
+
+            Console.ReadLine();
+        }
+    }
+
+}
